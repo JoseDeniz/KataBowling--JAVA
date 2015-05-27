9b064859b5d32e78f701aada52d95295a532fa44
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-05-27 23:35:13 +0100

    Refactor names

--- test/bowling/ScoreCalculatorShould.java
+++ test/bowling/ScoreCalculatorShould.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-public class AnAcceptedGame {
-    
+public class ScoreCalculatorShould {
+
+    public static final String ALL_STRIKES = "XXXXXXXXXXXX";
+
     @Test
     public void should_have_three_hundred_points_when_all_frames_in_the_line_are_strikes () {
-        ScoreCalculator scoreCalculator = new ScoreCalculator(new Line("XXXXXXXXXXXX"));
+        ScoreCalculator scoreCalculator = new ScoreCalculator(new Line(ALL_STRIKES));
         assertThat(scoreCalculator.score(), is(300));
     }
 }
