2d15f60b18926450410dbc67edc9a816aa23a93a
Jose Manuel Deniz Suarez <josema13992@gmail.com>
2015-05-29 00:45:26 +0100

    FrameBuilder builds Frames with Strikes and Spares

--- test/bowling/FrameBuilderShould.java
+++ test/bowling/FrameBuilderShould.java
@@ -2,6 +2,8 @@ package bowling;
 
 import org.junit.Test;
 
+import static bowling.FrameType.SPARE;
+import static bowling.FrameType.STRIKE;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -9,6 +11,8 @@ public class FrameBuilderShould {
 
     private static final Frame ZERO_PINS_FRAME = new Frame('-', '-');
     private static final Frame REGULAR_FRAME = new Frame('2', '3');
+    private static final Frame SPARE_FRAME = new Frame('2', '/');
+    private static final Frame STRIKE_FRAME = new Frame('X');
 
     @Test
     public void build_a_frame_with_zero_rolls() {
@@ -20,6 +24,20 @@ public class FrameBuilderShould {
     public void build_a_frame_with_a_regular_rolls() {
         Frame frame = FrameBuilder.build('2', '3');
         assertThat(frame, is(REGULAR_FRAME));
+    }
+
+    @Test
+    public void build_a_frame_with_a_spare() {
+        Frame frame = FrameBuilder.build('2', '/');
+        assertThat(frame, is(SPARE_FRAME));
+        assertThat(frame.type(), is(SPARE));
+    }
+
+    @Test
+    public void build_a_frame_with_a_strike() {
+        Frame frame = FrameBuilder.build('X');
+        assertThat(frame, is(STRIKE_FRAME));
+        assertThat(frame.type(), is(STRIKE));
     }
 
 }
